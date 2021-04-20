131446ee0a5c13263a9af21984e4916e5ba706f6
Divyanshu Yadav <thedivyanshuyadav@gmail.com>
2021-04-21 00:04:06 +0530

    Update settings.py

--- djangoImageProject/settings.py
+++ djangoImageProject/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
